8ee39656145ab84e2a34c8a902142a0b755b6890
Zach Taylor <zach@ztaylor.me>
2020-07-21 20:20:01 -0500

    breakout move
    Signed-off-by: Zach Taylor <zach@ztaylor.me>

---
+++ move/move.go
@@ -0,0 +1,18 @@
+package move
+
+type T struct {
+	ID                 int
+	Name               string
+	Type               int
+	Category           string
+	Priority           int
+	Description        string
+	PP                 int
+	Power              int
+	Accuracy           float64
+	Target             int
+	ContestCategory    int
+	ContestDescription string
+	Appeal             int
+	Jam                int
+}
